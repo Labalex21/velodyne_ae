e578176d39f9730456bbbf0024e6b97f3a39e820
Labalex21 <labalex21@gmail.com>
2018-03-28 14:18:45 +0200

    Update apply_conv_autoencoder_velodyne.py

--- apply_conv_autoencoder_velodyne.py
+++ apply_conv_autoencoder_velodyne.py
@@ -163,8 +163,8 @@ def export_encoder():
             for j in range(imgs.shape[0]):
                 string_img = dir_imgs + "img_" + str(j+start_idx) + ".png"
                 string_pred = dir_pred + "img_" + str(j+start_idx) + ".png"
-                cv2.imwrite(string_img, imgs[0]/max_dist*255)
-                cv2.imwrite(string_pred, imgs[0]*255)
+                cv2.imwrite(string_img, imgs[j]/max_dist*255)
+                cv2.imwrite(string_pred, imgs[j]/max_dist*255)
 
 x, labels, number_batches = fh.read_tfrecord(dir_records, image_shape, batch_size = batch_size,num_epochs=epochs)
 
