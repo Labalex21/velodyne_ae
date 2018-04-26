e65c035efe0c8cdf839cb28f0bf30ef2ecaa7c2d
Labalex21 <labalex21@gmail.com>
2018-04-26 09:55:26 +0200

    Update apply_conv_autoencoder_velodyne.py

--- apply_conv_autoencoder_velodyne.py
+++ apply_conv_autoencoder_velodyne.py
@@ -226,7 +226,7 @@ def export_encoder_csv(path_data, path_export, path_current_traj):
             imgs = []
             for j in range(start_idx,end_idx):
                 idx = int(traj[j,0])
-                img,_ = fh.get_velodyne_img(filenames[idx])
+                img,_ = fh.get_velodyne_img_csv(filenames[idx])
                 #img = img[:,:,0]/max_dist
                 #img = np.reshape(img,[img.shape[0],img.shape[1],1])
                 imgs.append(img)
