18430cdef2b75f7cddb1eacf4ddb1a64526af30d
Labalex21 <labalex21@gmail.com>
2018-05-04 08:29:49 +0200

    flush log

--- conv_autoencoder_velodyne.py
+++ conv_autoencoder_velodyne.py
@@ -337,8 +337,12 @@ for i in range(fc_array.shape[0]):
     cluster_size = 200
     sequence_length = 200
     log_file.write("sequence analysis...\n")
+    log_file.flush()
     compl, acc = seq.get_results(dir_export_20180201, path_array_ref,cluster_size,sequence_length)
     log_file.write("Done.\n")
+    log_file.flush()
     current_string = "features: " + str(n_features) + " patch size" + str(patch_size) + " " + str(fc_size_array[i,0]) + " " + str(fc_size_array[i,1]) + " " + str(fc_size_array[i,2]) + " " + str(fc_array[i]) + "_" + str(number_of_conv) + " completeness: " + str(compl) + " | RMSE: " + str(acc) + "\n"
     log_file.write(current_string)
+    log_file.flush()
     res_file.write(current_string)
+    res_file.flush()
