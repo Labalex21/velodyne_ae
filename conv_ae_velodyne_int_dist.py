49df4c77091410f9e32520230ca57d6cabbc53eb
Labalex21 <labalex21@gmail.com>
2018-05-06 11:24:54 +0200

    last_encoder_width

--- conv_ae_velodyne_int_dist.py
+++ conv_ae_velodyne_int_dist.py
@@ -167,7 +167,7 @@ def train():
         save_path = saver.save(sess, path_model)
         print("Model saved in file: %s" % save_path)
 
-def export_encoder(path_data, path_export, path_current_traj):
+def export_encoder(path_data, path_export, path_current_traj,last_encoder_width):
     # get trajectory
     traj = fh.get_scan_trajectory(path_current_traj)
     
@@ -178,7 +178,7 @@ def export_encoder(path_data, path_export, path_current_traj):
     number_of_scans = filenames.shape[0]
     
     # save feature values here
-    encoder_values = np.zeros((number_of_scans, last_encoder_width))
+    encoder_values = np.zeros((int(number_of_scans), int(last_encoder_width)))
     k = 1
     if number_of_scans % 100 == 0:
         k = 0
@@ -223,7 +223,7 @@ def export_encoder(path_data, path_export, path_current_traj):
                 #cv2.imwrite(string_img, imgs[j]*255)
                 #cv2.imwrite(string_pred, pred[j]*255)
                 
-def export_encoder_csv(path_data, path_export, path_current_traj):
+def export_encoder_csv(path_data, path_export, path_current_traj,last_encoder_width):
     # get trajectory
     traj = fh.get_scan_trajectory_csv(path_current_traj)
     
@@ -237,7 +237,7 @@ def export_encoder_csv(path_data, path_export, path_current_traj):
     number_of_scans = traj.shape[0]
     
     # save feature values here
-    encoder_values = np.zeros((number_of_scans, last_encoder_width))
+    encoder_values = np.zeros((int(number_of_scans), int(last_encoder_width)))
     k = 1
     if number_of_scans % 100 == 0:
         k = 0
@@ -287,6 +287,7 @@ for i in range(fc_array.shape[0]):
     number_of_fc = fc_array[i]
     path_model = "../data/20180201/models/conv_ae_velodyne_int_dist_" + str(fc_size_array[i,0]) + "_" + str(fc_size_array[i,1]) + "_" + str(fc_size_array[i,2]) + "_" + str(number_of_fc) + "_" + str(number_of_conv) + ".ckpt"
     dir_test = "../data/imgs/result_ae/fc/" + str(i) + "/"
+    last_encoder_width = fc_size_array[i,number_of_fc-1]
     
     # Reset graph
     tf.reset_default_graph()
@@ -310,12 +311,12 @@ for i in range(fc_array.shape[0]):
     path_traj = '../data/traj/scan_traj_20180201.txt'
     dir_export_20180201 = '../data/features/velodyne_20180201_int_dist_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data = '../data/20180201/scans_csv/'
-    export_encoder_csv(dir_data, dir_export_20180201, path_traj)
+    export_encoder_csv(dir_data, dir_export_20180201, path_traj,last_encoder_width)
 
     path_traj = '../data/traj/scan_traj_20180410_2.txt'
     dir_export_20180410_2 = '../data/features/velodyne_20180410_2_int_dist_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data = '../data/20180410/scans_rot_2/'
-    export_encoder(dir_data, dir_export_20180410_2, path_traj)
+    export_encoder(dir_data, dir_export_20180410_2, path_traj,last_encoder_width)
 
     dir_export_icsens = '../data/features/velodyne_icsens_int_dist_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data_icsens = "../data/20180201/scans_icsens/"
