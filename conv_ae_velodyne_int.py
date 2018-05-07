dff58f8e1b43310ae1b6875460097a98470ba3ad
Labalex21 <labalex21@gmail.com>
2018-05-07 10:44:44 +0200

    Update conv_ae_velodyne_int.py

--- conv_ae_velodyne_int.py
+++ conv_ae_velodyne_int.py
@@ -255,7 +255,7 @@ def export_encoder_csv(path_data, path_export, path_current_traj,last_encoder_wi
             for j in range(start_idx,end_idx):
                 idx = int(traj[j,0])
                 _,img = fh.get_velodyne_img_csv(filenames[idx])
-                img = img[:,:,0]
+                #img = img[:,:,0]
                 img = np.reshape(img,[img.shape[0],img.shape[1],1])
                 imgs.append(img)
             imgs = np.array(imgs)
