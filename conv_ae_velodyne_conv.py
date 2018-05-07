70f8791c91032b00951a1ada7efb0d428b4bac0a
Labalex21 <labalex21@gmail.com>
2018-05-07 10:43:18 +0200

    Update conv_ae_velodyne_conv.py

--- conv_ae_velodyne_conv.py
+++ conv_ae_velodyne_conv.py
@@ -318,7 +318,7 @@ for i in range(1,fc_array.shape[0]):
     path_traj = '../data/traj/scan_traj_20180410_2.txt'
     dir_export_20180410_2 = '../data/features/velodyne_20180410_2_conv_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data = '../data/20180410/scans_rot_2/'
-    #export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
+    export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
 
     dir_export_icsens = '../data/features/velodyne_icsens_conv_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data_icsens = "../data/20180201/scans_icsens/"
