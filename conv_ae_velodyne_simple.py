94dea06fb6c47a79ce6a636606495fd83e9bdd47
Labalex21 <labalex21@gmail.com>
2018-05-10 14:06:55 +0200

    Update conv_ae_velodyne_simple.py

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -200,7 +200,7 @@ def train():
         current_string = "init global" + "\n"
         log_file.write(current_string)
         log_file.flush()
-        #sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer())
         current_string = "init local" + "\n"
         log_file.write(current_string)
         log_file.flush()
