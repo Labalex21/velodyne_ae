f000d5d08cc2d410f6093d862838b84ffa23ee6f
Labalex21 <labalex21@gmail.com>
2018-05-10 13:23:06 +0200

    Update conv_ae_velodyne_simple.py

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -338,7 +338,7 @@ for i in range(1,fc_array.shape[0]):
 
     # optimizer
     optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
-    current_string = "train + "\n"
+    current_string = "train" + "\n"
     log_file.write(current_string)
     log_file.flush()
     #train
