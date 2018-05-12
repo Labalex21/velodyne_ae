9bda49bc42e956b852129458654f4768054e0846
Labalex21 <labalex21@gmail.com>
2018-05-12 11:06:06 +0200

    Use correct scans! Not csv

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -382,7 +382,7 @@ current_string = "before loop\n"
 log_file.write(current_string)
 log_file.flush()
 
-for i in range(2,fc_array.shape[0]):
+for i in range(1,fc_array.shape[0]):
     current_string = "in loop\n"
     log_file.write(current_string)
     log_file.flush()
@@ -426,13 +426,13 @@ for i in range(2,fc_array.shape[0]):
     # export encoder    
     path_traj = '../data/traj/scan_traj_20180201.txt'
     dir_export_20180201 = '../data/features/velodyne_20180201_simple_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
-    dir_data = '../data/20180201/scans_csv/'
-    export_encoder_csv(dir_data, dir_export_20180201, path_traj, last_encoder_width)
+    dir_data = '../data/20180201/scans/'
+    export_encoder(dir_data, dir_export_20180201, path_traj, last_encoder_width)
 
     path_traj = '../data/traj/scan_traj_20180410_2.txt'
     dir_export_20180410_2 = '../data/features/velodyne_20180410_2_simple_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data = '../data/20180410/scans_rot_2/'
-    export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
+    #export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
 
     dir_export_icsens = '../data/features/velodyne_icsens_simple_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data_icsens = "../data/20180201/scans_icsens/"
