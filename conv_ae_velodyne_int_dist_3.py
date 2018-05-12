efc13837f8f7165abb41be063f2980cd17c6c84c
Labalex21 <labalex21@gmail.com>
2018-05-12 11:12:32 +0200

    Update conv_ae_velodyne_int_dist_3.py

--- conv_ae_velodyne_int_dist_3.py
+++ conv_ae_velodyne_int_dist_3.py
@@ -440,8 +440,8 @@ for i in range(1,fc_array.shape[0]):
     # export encoder    
     path_traj = '../data/traj/scan_traj_20180201.txt'
     dir_export_20180201 = '../data/features/velodyne_20180201_dist_int_3_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
-    dir_data = '../data/20180201/scans_csv/'
-    export_encoder_csv(dir_data, dir_export_20180201, path_traj, last_encoder_width)
+    dir_data = '../data/20180201/scans/'
+    export_encoder(dir_data, dir_export_20180201, path_traj, last_encoder_width)
 
     path_traj = '../data/traj/scan_traj_20180410_2.txt'
     dir_export_20180410_2 = '../data/features/velodyne_20180410_2_dist_int_3_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
