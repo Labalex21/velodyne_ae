cdae5a611b8088a63edf711b0b6a88c182da8eb1
Labalex21 <labalex21@gmail.com>
2018-05-16 10:04:59 +0200

    Update conv_ae_velodyne_simple.py

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -383,7 +383,7 @@ current_string = "before loop\n"
 log_file.write(current_string)
 log_file.flush()
 
-for i in range(1,fc_array.shape[0]):
+for i in range(2,fc_array.shape[0]):
     
     current_string = "in loop\n"
     log_file.write(current_string)
