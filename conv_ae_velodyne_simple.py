9484bd11e88dff6217ca23ecb8719352e46cdf9c
Labalex21 <labalex21@gmail.com>
2018-05-24 16:56:30 +0200

    Only export 20180201

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -428,7 +428,7 @@ current_string = "before loop\n"
 log_file.write(current_string)
 log_file.flush()
 
-for i in range(2,fc_array.shape[0]):
+for i in range(0,fc_array.shape[0]):
     
     current_string = "in loop\n"
     log_file.write(current_string)
@@ -466,7 +466,7 @@ for i in range(2,fc_array.shape[0]):
     log_file.write(current_string)
     log_file.flush()
     #train
-    train()
+    #train()
     current_string = "Export" + "\n"
     log_file.write(current_string)
     log_file.flush()
@@ -481,7 +481,7 @@ for i in range(2,fc_array.shape[0]):
     path_traj = '../data/traj/scan_traj_20180410_2.txt'
     dir_export_20180410_2 = '../data/features/velodyne_20180410_2_simple_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data = '../data/20180410/scans_rot_2/'
-    export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
+    #export_encoder(dir_data, dir_export_20180410_2, path_traj, last_encoder_width)
 
     dir_export_icsens = '../data/features/velodyne_icsens_simple_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '.json'
     dir_data_icsens = "../data/20180201/scans_icsens/"
