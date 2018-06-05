e937c5dee9337f2b34b56a830eb280cbe0944548
Labalex21 <labalex21@gmail.com>
2018-06-05 10:49:54 +0200

    Update conv_ae_velodyne_int.py

--- conv_ae_velodyne_int.py
+++ conv_ae_velodyne_int.py
@@ -351,9 +351,9 @@ def export_encoder_npy(path_data, path_export, path_current_traj, last_encoder_w
                 log_file.write(current_string)
                 log_file.flush()
             
-    if path_current_traj is '../data/traj/scan_traj_20180410_2.txt':
+    if path_current_traj == '../data/traj/scan_traj_20180410_2.txt':
         encoder_values = np.delete(encoder_values,np.arange(8150,8600),axis=0)
-        trajectory = np.delete(trajectory,np.arange(8150,8600),axis=0)
+        traj = np.delete(traj,np.arange(8150,8600),axis=0)
     
     # export values to json file
     with open(path_export, 'w') as f:
@@ -463,7 +463,7 @@ for i in range(2,fc_array.shape[0]):
     log_file.write(current_string)
     log_file.flush()
     #train
-    train()
+    #train()
     current_string = "Export" + "\n"
     log_file.write(current_string)
     log_file.flush()
