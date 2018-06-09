ed04e0703b52d95fe3d454e6581f86850011a9c0
Labalex21 <labalex21@gmail.com>
2018-06-09 15:12:54 +0200

    2 fully connected layer

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -155,8 +155,8 @@ def create_network(x_input, number_fc, fc_widths):
     encoder = fc1
 
     # 2nd fully connected layer
-    # fc2 = fully_connected(fc1, weights['wfc2'], biases['b2_enc'])
-    # print('fc2: ', fc2.get_shape())
+    fc2 = fully_connected(fc1, weights['wfc2'], biases['b2_enc'])
+    print('fc2: ', fc2.get_shape())
 
     # 3rd fully connected layer --> encoder values
     # fc3 = fully_connected(fc2, weights['wfc3'], biases['b3_enc'])
@@ -168,11 +168,11 @@ def create_network(x_input, number_fc, fc_widths):
     # print('tfc1: ', tfc1.get_shape())
 
     # 2nd fully connected layer of decoder
-    # tfc2 = fully_connected(fc2, tf.transpose(weights['wfc2']), biases['b1_dec'])
-    # print('tfc2: ', tfc2.get_shape())
+    tfc2 = fully_connected(fc2, tf.transpose(weights['wfc2']), biases['b1_dec'])
+    print('tfc2: ', tfc2.get_shape())
 
     # 3rd and last fully connected layer of decoder
-    tfc3 = fully_connected(fc1, tf.transpose(weights['wfc1']), biases['b3_dec'])
+    tfc3 = fully_connected(tfc2, tf.transpose(weights['wfc1']), biases['b3_dec'])
     tfc3 = tf.reshape(tfc3, [-1,900, 16, n_features])
     # tfc3 = tf.reshape(tfc3, [-1,2 , 113, n_features])
     print('tfc3: ', tfc3.get_shape())
@@ -435,7 +435,7 @@ def export_encoder_csv(path_data, path_export, path_current_traj, last_encoder_w
     with open(path_export, 'w') as f:
         json.dump({"encoder": encoder_values.tolist(), "trajectory": traj.tolist()}, f)
 
-fc_array = np.array([1,1,1,1,1,1])
+fc_array = np.array([2,2,2,2,2,2])
 fc_size_array = np.array([[800,400,50],
                  [400,200,50],
                  [200,100,50],
@@ -485,8 +485,7 @@ for i in range(1,fc_array.shape[0]):
     log_file.write(current_string)
     log_file.flush()
     #train
-    if i > 1:
-        train()
+    train()
     current_string = "Export" + "\n"
     log_file.write(current_string)
     log_file.flush()
