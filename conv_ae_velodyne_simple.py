4c92a07d1a22138ab4eb36923454748937faa670
Labalex21 <labalex21@gmail.com>
2018-10-01 17:43:13 +0200

    Update conv_ae_velodyne_simple.py

--- conv_ae_velodyne_simple.py
+++ conv_ae_velodyne_simple.py
@@ -544,12 +544,12 @@ for i in range(3,4):#fc_size_array.shape[0]):
     path_traj = '../data/traj/scan_traj_20180913.txt'
     dir_export_20180913 = '../data/features/velodyne_20180913_anne_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '_' +  str(3) + '.json'
     dir_data = '../data/20180913/scans_npy/'
-    #export_encoder_npy(dir_data, dir_export_20180913, path_traj, last_encoder_width)
+    export_encoder_npy(dir_data, dir_export_20180913, path_traj, last_encoder_width)
     
     path_traj = '../data/traj/scan_traj_20180926.txt'
     dir_export_20180926 = '../data/features/velodyne_20180926_anne_' + str(last_encoder_width) + '_' +  str(number_of_fc) + '_' +  str(number_of_conv) + '_' +  str(3) + '.json'
     dir_data = '../data/20180926/scans_npy/'
-    #export_encoder_npy(dir_data, dir_export_20180926, path_traj, last_encoder_width)
+    export_encoder_npy(dir_data, dir_export_20180926, path_traj, last_encoder_width)
     
    
     #path_array_ref = [dir_export_20180201, dir_export_20180531, dir_data_icsens, dir_data_herrenhausen, dir_export_leinhausen, dir_export_stoecken]
